2588755ca8ce1761b20ba1c5c9c31ce8fd197b69
yhada <yhada@techloco.co.jp>
2019-05-09 16:54:32 +0900

    feat: animation01.

--- main.js
+++ main.js
@@ -1,10 +1,49 @@
 
-var animation = anime({
-    targets: '#pointer',
-    translateX: 250,
+// opacityプロパティはデフォルトのパラメータに設定できない
+var animation = anime.timeline({
     easing: 'linear',
-    duration: 300,
+    duration: 500,
     loop: false,
+})
+
+// HACK: 冗長な書き方
+animation.add({
+    targets: '#pointer1',
+    translateY: -150,
+    opacity: 1,
+})
+animation.add({
+    targets: '#pointer2',
+    translateX: 150,
+    translateY: -150,
+    opacity: 1,
+})
+animation.add({
+    targets: '#pointer3',
+    translateX: 150,
+    opacity: 1,
+})
+animation.add({
+    targets: '#pointer4',
+    translateY: 150,
+    translateX: 150,
+    opacity: 1,
+})
+animation.add({
+    targets: '#pointer5',
+    translateY: 150,
+    opacity: 1,
+})
+animation.add({
+    targets: '#pointer6',
+    translateY: 150,
+    translateX: -150,
+    opacity: 1,
+})
+animation.add({
+    targets: '#pointer7',
+    translateX: -150,
+    opacity: 1,
 })
 
 const pause = () => {
